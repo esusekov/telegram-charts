6962117ed30cc37c7527b59f8d432b7449bc8f86
Egor Susekov <esusekov@gmail.com>
2019-03-20 00:45:32 +0300

    update

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import resolve from 'rollup-plugin-node-resolve'
 export default {
 	input: 'src/main.js',
 	output: {
-		file: 'public/bundle.js',
+		file: 'bundle.js',
 		format: 'iife',
 	},
 	plugins: [
